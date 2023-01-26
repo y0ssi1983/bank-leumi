e02a5ea22b86f45d4c97c4fd0a22862f3a8a62c1
y0ssi1983 <mz.yosi@gmail.com>
2023-01-26 09:32:04 +0000

    change hello.py sentence

--- hello-bank/hello.py
+++ hello-bank/hello.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
   return {
     'statusCode': 200,
-    'body': json.dumps('Hello Bank Leumi, I want to join you')
+    'body': json.dumps('Hi Bank Leumi, Let\'s work together :)')
     }
